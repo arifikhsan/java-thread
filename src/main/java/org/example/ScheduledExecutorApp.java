ae6930862d5799fe5e10a2bfc2ccba7f4134a5c2
Arif Ikhsanudin <arif.ikhsanudin@mitrais.com>
2022-06-29 14:52:36 +0700

    scheduled executor service

---
+++ src/main/java/org/example/ScheduledExecutorApp.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorApp {
+    public static void main(String[] args) throws InterruptedException {
+        // delayedJob();
+        periodicJob();
+    }
+
+    public static void delayedJob() {
+        var executor  = Executors.newScheduledThreadPool(10);
+        var future = executor.schedule(() -> System.out.println("Hello"), 5, TimeUnit.SECONDS);
+        System.out.println(future.getDelay(TimeUnit.MILLISECONDS));
+        executor.shutdown();
+    }
+
+    public static void periodicJob() throws InterruptedException {
+        var executor  = Executors.newScheduledThreadPool(10);
+        var future = executor.scheduleAtFixedRate(() -> System.out.println("Hello"), 2, 2, TimeUnit.SECONDS);
+        System.out.println(future.getDelay(TimeUnit.MILLISECONDS));
+
+        Thread.sleep(5000);
+        executor.shutdown();
+    }
+}
